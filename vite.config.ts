da85e4dd1091b98d584c05ce4d728e8670d57230
siim8n-cmd <siim8n@gmail.com>
2025-11-10 14:13:11 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: mode === 'production' ? '/balder/' : '/', // 👈 kun i production
-}))
+  base: '/', // FORCE this to '/' for Vercel deployment
+})
